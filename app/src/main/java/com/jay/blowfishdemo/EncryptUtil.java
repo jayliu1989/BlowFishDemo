6cff33c0e0ea06667d93996d656ddb55df9b6fec
jayliu1989 <jayliu1989@live.cn>
2019-07-24 22:17:17 +0800

    1,fix decrypt bug

--- app/src/main/java/com/jay/blowfishdemo/EncryptUtil.java
+++ app/src/main/java/com/jay/blowfishdemo/EncryptUtil.java
@@ -9,13 +9,13 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class EncryptUtil {
-
+    private static final String ALGORITHM = "Blowfish";
+    private static final String TAG = EncryptUtil.class.getSimpleName();
 
     public static String encrypt(String to_encrypt, String key) {
         try {
-            KeyGenerator keygenerator = KeyGenerator.getInstance("Blowfish");
-            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "Blowfish");
-            Cipher cipher = Cipher.getInstance("Blowfish/CBC/PKCS5Padding");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
             byte[] hasil = cipher.doFinal(to_encrypt.getBytes());
             return Base64.encodeToString(hasil, Base64.DEFAULT);
@@ -26,15 +26,13 @@ public class EncryptUtil {
 
     public static String decrypt(String to_decrypt, String key) {
         try {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "Blowfish");
-            Cipher cipher = Cipher.getInstance("Blowfish/CBC/PKCS5Padding");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-//            byte[] hasil = cipher.doFinal(to_decrypt.getBytes());
             byte[] hasil = cipher.doFinal(Base64.decode(to_decrypt, Base64.DEFAULT));
             return new String(hasil);
-//            return Base64.encodeToString(hasil, Base64.DEFAULT);
         } catch (Exception e) {
-            Log.e("TAG", e.getMessage());
+            Log.e(TAG, e.getMessage());
             return null;
         }
     }
